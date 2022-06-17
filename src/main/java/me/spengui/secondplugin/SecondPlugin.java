fa643f0634a289b01c32c15c5552af12dce1ead0
SpenGUI <spencersinclaircard@gmail.com>
2022-06-17 23:02:01 +0100

    First Commit

---
+++ src/main/java/me/spengui/secondplugin/SecondPlugin.java
@@ -0,0 +1,17 @@
+package me.spengui.secondplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SecondPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
